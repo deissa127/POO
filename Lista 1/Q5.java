7685ce4f17aa7c9889ae13d12a7b3921ce981588
Hadassa dos Santos Medeiros <Hadassadosmedeiros@gmail.com>
2026-04-08 18:54:47 +0000

    q5 incompleto

---
+++ Lista 1/Q5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        double delta = (B*B) - 4 * A * C;
+
+        if (a == 0 && b == 1){
+            System.out.println("C");
+        } else if (a ==  && b == 2){
+            System.out.println("C");
+        }
+        scanner.close();
+    }
+}
